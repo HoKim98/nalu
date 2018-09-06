c305c05f70d5f265183d642e1f37871f4d21e34d
kerryeon <besqer996@gnu.ac.kr>
2018-09-06 16:04:10 +0900

    Update Mayan, TrainManager, etc.

--- test/language_to_number.py
+++ test/language_to_number.py
@@ -1,40 +1,29 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 from torch.nn.init import xavier_uniform_
-
 from model.layer.nalu import NaluLayer
-from utils.data_manager import DataManager
-
-
-from lang.num.roman import Num
-#from lang.num.en import Num
-#from lang.num.ko_kr import Num
-data_util = DataManager(Num)
 
 
 class Model(nn.Module):
 
-    def __init__(self, num_hidden: int = 32, num_lstm_layers: int = 1):
+    def __init__(self, num_embedding, num_hidden=32, num_lstm_layers=1, use_nalu=True, reduce_sum=True):
         super(Model, self).__init__()
         self.num_hidden = num_hidden
         self.num_layers = num_lstm_layers
+        self.reduce_sum = reduce_sum
 
-        self.embedder = nn.Embedding(len(data_util), num_hidden)
+        self.embedder = nn.Embedding(num_embedding, num_hidden)
         self.lstm = nn.LSTM(num_hidden, num_hidden, num_lstm_layers)
-
-        #self.final = nn.Linear(num_hidden, 1)
-        self.final = NaluLayer(num_hidden, 1, 1, 0)
+        self.final = NaluLayer(num_hidden, 1, 1, 0) if use_nalu else nn.Linear(num_hidden, 1)
 
         self.hidden = None
-        with torch.no_grad():
+        '''with torch.no_grad():
             for w in self.lstm.parameters():
                 if w.dim() == 2:
                     xavier_uniform_(w)
                 else:
                     w.fill_(0)
-            xavier_uniform_(self.embedder.weight)
+            xavier_uniform_(self.embedder.weight)'''
 
     def init_hidden(self, batch_size=1):
         self.hidden = (torch.zeros(self.num_layers, batch_size, self.num_hidden),
@@ -44,87 +33,43 @@ class Model(nn.Module):
         self.init_hidden(1)
         x = self.embedder(x)
         x, self.hidden = self.lstm(x, self.hidden)
-        #x = x.mean(dim=0)
-        x = x[-1]
+        x = x.sum(dim=0) if self.reduce_sum else x[-1]
         x = self.final(x)
         return x
 
 
-data_train, data_val, data_eval = data_util.divide(data_util.generate())
+if __name__ == '__main__':
 
+    import argparse
 
-model = Model()
-op = optim.Adam(model.parameters())
-lr_drop = optim.lr_scheduler.ReduceLROnPlateau(op)
+    import torch.optim as optim
+    from utils.data_manager import DataManager
+    from utils.train_manager import TrainManager
 
+    parser = argparse.ArgumentParser(description='NALU - Language to Number Translation Tasks')
+    parser.add_argument('--lang', type=str, default='en', choices=['en', 'ko', 'ja', 'roman', 'mayan'])
+    parser.add_argument('--hidden', type=int, default=32, choices=[16, 32])
+    parser.add_argument('--lstm-layers', type=int, default=1, choices=[1, 2])
+    parser.add_argument('--use-nalu', type=int, default=1, choices=[0, 1])
+    parser.add_argument('--lr', type=float, default=1e-2, choices=[1e-2, 1e-3])
+    parser.add_argument('--reduce-sum', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--reduce-lr', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--epochs', type=int, default=591)
+    args = parser.parse_args()
 
-def train(i):
-    global data_train
-    N = len(data_train[0])
-    loss_sum, accuracy = 0., 0
-    for x, y in zip(*data_train):
-        x = torch.LongTensor(x)
-        x = x.unsqueeze(1)
-        y_ = model(x)
+    Num = __import__('lang.num.%s' % args.lang, fromlist=['Num'])
+    data_manager = DataManager(getattr(Num, 'Num'))
 
-        loss = (y_ - y).pow(2).mean()
-        if not torch.isnan(y_):
-            op.zero_grad()
-            loss.backward()
-            op.step()
+    # TODO: Support more 'divide' option - now only English available
+    data_train, data_val, data_eval = data_manager.divide(data_manager.generate())
 
-        loss_sum += float(loss)
-        if not torch.isnan(y_):
-            accuracy += int(int(y_ + .5) == int(y))
+    model = Model(len(data_manager), args.hidden, args.lstm_layers, args.use_nalu, args.reduce_sum)
+    op = optim.Adam(model.parameters(), lr=args.lr)
+    trainer = TrainManager(model, op, data_manager, args.reduce_lr, data_train, data_val, data_eval)
 
-    loss_sum, accuracy = loss_sum / N, accuracy * 100 / N
-    #print('Train MSE ', i, loss_sum, accuracy)
-    data_train = data_util.shuffle(data_train)
-
-
-def validate(i):
-    N = len(data_val[0])
-    loss_sum, accuracy = 0., 0
-    for x, y in zip(*data_val):
-        x = torch.LongTensor(x)
-        x = x.unsqueeze(1)
-        y_ = model(x)
-
-        loss = (y_ - y).abs().mean()
-
-        loss_sum += float(loss)
-        if not torch.isnan(y_):
-            accuracy += int(int(y_ + .5) == int(y))
-
-    loss_sum, accuracy = loss_sum / N, accuracy * 100 / N
-    #print('Valid MAE ', i, loss_sum, accuracy)
-    lr_drop.step(loss_sum, i)
-
-
-def eval(i):
-    N = len(data_eval[0])
-    loss_sum, accuracy = 0., 0
-    for x, y in zip(*data_eval):
-        x = torch.LongTensor(x)
-        x = x.unsqueeze(1)
-        y_ = model(x)
-
-        loss = (y_ - y).abs().mean()
-
-        loss_sum += float(loss)
-        if not torch.isnan(y_):
-            accuracy += int(int(y_ + .5) == int(y))
-
-    loss_sum, accuracy = loss_sum / N, accuracy * 100 / N
-    print('Eval  MAE ', i, loss_sum, accuracy)
-    global loss_min
-    if loss_sum == loss_sum:
-        loss_min = min(loss_min, loss_sum)
-
-
-loss_min = 1.e+9
-for i in range(3000):
-    train(i)
-    validate(i)
-    eval(i)
-print('min', loss_min)
+    loss_min = 1.e+9
+    for i in range(args.epochs):
+        trainer.train(verbose=True)
+        trainer.validate()
+        loss_min = min(loss_min, trainer.eval())
+    print('min', loss_min)
